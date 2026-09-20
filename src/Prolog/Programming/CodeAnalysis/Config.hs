57f57a9e569a53cb7b48cf36bae0f9079be41c33
nimec01 <24428341+nimec01@users.noreply.github.com>
2026-09-20 18:21:21 +0200

    remove unused import

--- src/Prolog/Programming/CodeAnalysis/Config.hs
+++ src/Prolog/Programming/CodeAnalysis/Config.hs
@@ -9,7 +9,7 @@ where
 import Data.List (singleton)
 import Prolog.Programming.CodeAnalysis.Rules.NoSingletonVariables (noSingletonVariablesRule)
 import Prolog.Programming.CodeAnalysis.Rules.RestrictCutUsage (restrictCutUsageRule)
-import Prolog.Programming.CodeAnalysis.Types (CodeAnalysisConfig (..), ConfiguredRule (..), Rule (..), Severity (..))
+import Prolog.Programming.CodeAnalysis.Types (CodeAnalysisConfig (..), ConfiguredRule (..), Severity (..))
 
 configuredRules :: CodeAnalysisConfig -> [ConfiguredRule]
 configuredRules CodeAnalysisConfig {..} =
