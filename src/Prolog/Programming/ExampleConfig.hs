7ab2342deb7d0bb2c1f52ea81f21297ea238972b
nimec01 <24428341+nimec01@users.noreply.github.com>
2026-09-22 15:31:34 +0200

    update example config

--- src/Prolog/Programming/ExampleConfig.hs
+++ src/Prolog/Programming/ExampleConfig.hs
@@ -7,39 +7,45 @@ import qualified Text.RawString.QQ as RS (r)
 
 exampleConfig :: Config
 exampleConfig = Config
-  [RS.r|/* % comments for test cases have to start with an extra %
- * % timeout per test in ms (defaults to 10000, only the first timeout is used)
- * Global timeout: 10000
- * % style of derivation tree rendering can be either 'query' or 'resolution' (defaults to 'query')
- * Tree style: query
- * % The two include options control if and how the hidden definitions and definitions from the task description should be included into the submission.
- * % 'yes': include as is,
- * % 'filtered':
- * %    - For hidden definitions all clauses for predicate pred/k are filtered out if the input program also contains clauses for pred/k,
- * %    - For task definitions clauses that occur identically in the input program are filtered out
- * % 'no': do not include.
- * % Both default to yes
- * % Include hidden definitions: [ yes | filtered ]
- * % Include task definitions: [ yes | filtered | no ]
- * % allow/disallow the use of [H|T] matching on list values, defaults to 'yes' (experimental: might not recognize all instances of the pattern)
- * % Allow list pattern matching: [ yes | no ]
- * % Show SWISH button: [ yes | no ]
- * % prefixing a test with [<time out in ms>] sets a local timeout for that test
- * a_predicate(Foo,Bar): a_predicate(expected_foo1,expected_bar1), a_predicate(expected_foo2,expected_bar2)
- * a_statement_that_has_to_be_true
- * !a_predicate_whose_answers_are_hidden(Foo,Bar): a_predicate(expected_foo1,expected_bar1), a_predicate(expected_foo2,expected_bar2)
- * !a_hidden_statement_that_has_to_be_true
- * !(<description>) a_hidden_statement_that_has_to_be_true_with_a_description_shown_on_failure
- * @a_test_with_resolution_tree(X) % Only shown if test fails.
- * -a_statement_that_has_to_be_false % also works for all other test statements given above
- * % when combining multiple flags the order has to be <timeout><negative><tree><hidden><space>*<test>
- * new a_predicate_to_define(X): predicate description
- *    % require the definition of a predicate with a user chosen name. Use a_predicate_to_define to refer this predicate in other tests.
- *    % New predicates will be mapped to required predicates in the order they are defined.
- *    % (The initial solution automatically provides comments helping the user with the correct ordering.)
- */
+  [RS.r|
+# timeout per test in ms (defaults to 10000, only the first timeout is used)
+globalTimeout: 1000
+
+# style of derivation tree rendering can be either 'query' or 'resolution' (defaults to 'query')
+treeStyle: query
+
+# The two include options control if and how the hidden definitions and definitions from the task description should be included into the submission.
+# 'yes': include as is,
+# 'filtered':
+#    - For hidden definitions all clauses for predicate pred/k are filtered out if the input program also contains clauses for pred/k,
+#    - For task definitions clauses that occur identically in the input program are filtered out
+# 'no': do not include.
+# Both default to yes
+# includeTaskDefinitions: yes
+# includeHiddenDefinitions: yes
+
+# allow/disallow the use of [H|T] matching on list values, defaults to 'yes' (true) (experimental: might not recognize all instances of the pattern)
+# allowListPatternMatching: true
+
+# showSWISHButton: false
+
+# prefixing a test with [<time out in ms>] sets a local timeout for that test
+specifications:
+  - "a_predicate(Foo,Bar): a_predicate(expected_foo1,expected_bar1), a_predicate(expected_foo2,expected_bar2)"
+  - "a_statement_that_has_to_be_true"
+  - "!a_predicate_whose_answers_are_hidden(Foo,Bar): a_predicate(expected_foo1,expected_bar1), a_predicate(expected_foo2,expected_bar2)"
+  - "!a_hidden_statement_that_has_to_be_true"
+  - "!(<description>) a_hidden_statement_that_has_to_be_true_with_a_description_shown_on_failure"
+  - "@a_test_with_resolution_tree(X)" # Only shown if test fails.
+  - "-a_statement_that_has_to_be_false" # also works for all other test statements given above
+  # when combining multiple flags the order has to be <timeout><negative><tree><hidden><space>*<test>
+  - "new a_predicate_to_define(X): predicate description"
+  # require the definition of a predicate with a user chosen name. Use a_predicate_to_define to refer this predicate in other tests.
+  # New predicates will be mapped to required predicates in the order they are defined.
+  # (The initial solution automatically provides comments helping the user with the correct ordering.)
+------------------------------
 /* Everything from here on (up to an optional hidden section separated by a line of 3 or more dashes)
- * will be part of the visible exercise description (In other words: Only the first comment block is special).
+ * will be part of the visible exercise description.
  *
  * You can add as many tests as you like, but keep Autotool's time limit in mind. Additionally, every test has its own time limit,
  * so if one of your tests does not terminate (soon enough) this will be reported as a failure (mentioning the timeout).
