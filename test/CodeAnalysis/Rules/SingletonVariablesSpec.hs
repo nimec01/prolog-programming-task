bb9f391a222de179512c6d945c426a642a7f47cf
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2026-09-25 08:19:44 +0200

    Additional tests

--- test/CodeAnalysis/Rules/SingletonVariablesSpec.hs
+++ test/CodeAnalysis/Rules/SingletonVariablesSpec.hs
@@ -38,36 +38,43 @@ spec = describe "NoSingletonVariables" $ do
   it "detects problem on example 3" $
     detectsProblem "p(X) :- X = [Z|Zs], q(Zs)."
 
-  it "doesn't detect problem on example 4" $
+  it "doesn't detect problems on example 4" $
     doesNotDetectProblem "p(X) :- q(X)."
-  it "doesn't detect problem on example 5" $
+  it "doesn't detect problems on example 5" $
     doesNotDetectProblem "p(X,X)."
-  it "doesn't detect problem on example 6" $
+  it "doesn't detect problems on example 6" $
     doesNotDetectProblem "p(X) :- X = [Z|Zs], q(Z,Zs)."
 
-  it "detect problem on example 7" $
+  it "detects problem on example 7" $
     detectsProblem "p :- a(X)."
-  it "doesn't detect problem on example 8" $
+  it "doesn't detect problems on example 8" $
     doesNotDetectProblem "p :- a(X), b(X)."
   it "detects problem on example 9" $
     detectsProblem "p(X) :- a(X), b(Y)."
-  it "doesn't detect problem on example 10" $
+  it "doesn't detect problems on example 10" $
     doesNotDetectProblem "p(X,Y) :- Z is X + Y, q(Z)."
-  it "doesn't detect problem on example 11" $
+  it "doesn't detect problems on example 11" $
     doesNotDetectProblem "p(X,Y) :- X =:= Y."
-  it "doesn't detect problem on example 12" $
+  it "doesn't detect problems on example 12" $
     doesNotDetectProblem "p(X,Y) :- X =\\= Y."
-  it "doesn't detect problem on example 13" $
+  it "doesn't detect problems on example 13" $
     doesNotDetectProblem "p(X,Y) :- not(X =:= Y)."
-  it "doesn't detect problem on example 14" $
+  it "doesn't detect problems on example 14" $
     doesNotDetectProblem "p(X,Y) :- X \\= Y."
   it "detects problem on example 15" $
     detectsProblem "p(X,f(Y)) :- g(X)."
-  it "doesn't detect problem on example 16" $
+  it "doesn't detect problems on example 16" $
     doesNotDetectProblem "p(X,Y) :- X > Y."
   it "doesn't detect problems on example 17" $
     doesNotDetectProblem "p(X,X,X)."
   it "doesn't detect problems on example 18" $
     doesNotDetectProblem "p(X,X,X,X)."
+
   it "doesn't detect problems on example 19" $
     doesNotDetectProblem "p(_)."
+  it "detects problems on example 20" $
+    detectsProblem "p(A,B)."
+  it "detects problem on example 21" $
+    detectsProblem "p(A,_)."
+  it "doesn't detect problems on example 22" $
+    doesNotDetectProblem "p(_A,_2)."
