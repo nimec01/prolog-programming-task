67a9fb37e28d6b5d7e292dffa12b146a79a117a8
nimec01 <24428341+nimec01@users.noreply.github.com>
2026-09-25 18:34:18 +0200

    use marked correctly

--- test/ConfigSpec.hs
+++ test/ConfigSpec.hs
@@ -10,5 +10,4 @@ spec = describe "ExampleConfig" $ do
   it "example config should be valid" $
     verifyConfig exampleConfig `shouldSatisfy` isJust
   it "rejects config with unknown fields" $
-    -- no-spell-check
-    verifyConfig (Config "globalTiimeout: 1000") `shouldSatisfy` isNothing
+    verifyConfig (Config "globalTiimeout: 1000") `shouldSatisfy` isNothing -- no-spell-check
