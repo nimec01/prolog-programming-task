095a58707f8b858348086dc751cc9f412d00dde5
nimec01 <24428341+nimec01@users.noreply.github.com>
2026-09-25 18:26:47 +0200

    add another config validation test

--- test/ExampleConfigSpec.hs
+++ test/ExampleConfigSpec.hs
@@ -1,6 +1,7 @@
 module ExampleConfigSpec where
 
-import Data.Maybe (isJust)
+import Data.Maybe (isJust, isNothing)
+import Prolog.Programming.Data (Config (..))
 import Prolog.Programming.Task (exampleConfig, verifyConfig)
 import Test.Hspec (Spec, describe, it, shouldSatisfy)
 
@@ -8,3 +9,5 @@ spec :: Spec
 spec = describe "ExampleConfig" $ do
   it "should be valid" $
     verifyConfig exampleConfig `shouldSatisfy` isJust
+  it "rejects config with unknown fields" $
+    verifyConfig (Config "globalTiimeout: 1000") `shouldSatisfy` isNothing
