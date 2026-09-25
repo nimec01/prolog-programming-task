5da9289c8310432d30e209802ba10e26c4d15cfd
nimec01 <24428341+nimec01@users.noreply.github.com>
2026-09-25 13:38:15 +0200

    Revert "extend singleton variable check"
    This reverts commit bab9846bac257d9f311b674d2ca961be26befcef.

--- src/Prolog/Programming/CodeAnalysis/Rules/SingletonVariables.hs
+++ src/Prolog/Programming/CodeAnalysis/Rules/SingletonVariables.hs
@@ -1,52 +1,17 @@
 module Prolog.Programming.CodeAnalysis.Rules.SingletonVariables (singletonVariablesRule) where
 
 import Data.Generics (Data, everything, mkQ)
-import Data.List (isPrefixOf)
 import Data.Map (Map)
-import qualified Data.Map as Map (empty, foldrWithKey, singleton, unionWith)
+import qualified Data.Map as Map (empty, filter, keys, singleton, unionWith)
 import Data.Text.Lazy (pack)
 import Language.Prolog (Clause (..), Term (..), VariableName (..))
 import Prolog.Programming.CodeAnalysis.Types (Problem (..), Rule)
-import Text.PrettyPrint.Leijen.Text (Doc, indent, linebreak, string, vsep)
+import Text.PrettyPrint.Leijen.Text (indent, linebreak, string, vsep)
 
 singletonVariablesRule :: Rule
-singletonVariablesRule clause =
-  map
-    ( \var ->
-        toProblem
-          clause
-          [ string (pack $ "includes the singleton variable " ++ var ++ ".") <> linebreak,
-            string $ pack "You can safely replace it with a wildcard (_)."
-          ]
-    )
-    shouldBeWildcard
-    ++ map
-      ( \var ->
-          toProblem
-            clause
-            [ string
-                ( pack $
-                    "includes singleton-marked variable "
-                      ++ var
-                      ++ " that is used more than once."
-                )
-                <> linebreak,
-              string $ pack "You should remove the underscore prefix."
-            ]
-      )
-      singletonMarkedMultiple
+singletonVariablesRule clause = map (toProblem clause) singletonVariables
   where
-    variableCounts = countVariables clause
-    (shouldBeWildcard, singletonMarkedMultiple) =
-      Map.foldrWithKey
-        update
-        ([], [])
-        variableCounts
-
-    update k v (a, b) = case ("_" `isPrefixOf` k, v) of
-      (False, 1) -> (k : a, b)
-      (True, v') | v' >= 2 -> (a, k : b)
-      _ -> (a, b)
+    singletonVariables = Map.keys . Map.filter (== 1) $ countVariables clause
 
 countVariables :: (Data a) => a -> Map String Int
 countVariables = everything (Map.unionWith (+)) $ mkQ Map.empty count
@@ -55,14 +20,15 @@ countVariables = everything (Map.unionWith (+)) $ mkQ Map.empty count
     count (Var (VariableName _ name)) = Map.singleton name 1
     count _ = Map.empty
 
-toProblem :: Clause -> [Doc] -> Problem
-toProblem clause desc =
+toProblem :: Clause -> String -> Problem
+toProblem clause var =
   Problem
     { problemClause = clause,
       problemDisplay =
-        vsep $
+        vsep
           [ string $ pack "Your clause",
-            indent 2 $ string $ pack $ show clause
+            indent 2 $ string $ pack $ show clause,
+            string (pack $ "includes the singleton variable " ++ var ++ ".") <> linebreak,
+            string $ pack "You can safely replace it with a wildcard (_)."
           ]
-            ++ desc
     }
